1f7e6367af1083076ddf0ebcbfbe9f4f2da639d8
Tgiti Abderrazak <abderrazak.tigiti@mce.gouv.qc.ca>
2022-02-08 12:29:30 +0100

    modify ext_emconf for supporting v11

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
         'depends' => [
             'typo3' => '10.4.0-11.5.99',
             'fluid_styled_content' => '10.4.0-11.5.99',
-            'rte_ckeditor' => '10.4.0-10.4.99',
+            'rte_ckeditor' => '10.4.0-11.5.99',
         ],
         'conflicts' => [
         ],
