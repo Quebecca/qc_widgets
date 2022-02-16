d08456fcfb4cef2d62677b8056182f3e41c6d2c8
Tgiti Abderrazak <abderrazak.tigiti@mce.gouv.qc.ca>
2022-02-16 18:54:21 +0100

    fix workspaces dependency version

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
             'typo3' => '10.4.0-11.5.99',
             'fluid_styled_content' => '10.4.0-11.5.99',
             'rte_ckeditor' => '10.4.0-11.5.99',
-            'workspaces' => '^11.5.99'
+            'workspaces' => '10.4.0-11.5.99'
         ],
         'conflicts' => [
         ],
