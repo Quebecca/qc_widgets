3e986a0509032aa300e3e5a33ca9ab1d4226a53a
Tgiti Abderrazak <abderrazak.tigiti@mce.gouv.qc.ca>
2022-06-30 12:52:05 +0100

    add file description for the class AdditionalCssImp

--- Classes/Widgets/AdditionalCssImp.php
+++ Classes/Widgets/AdditionalCssImp.php
@@ -1,5 +1,15 @@
 <?php
 
+/***
+ *
+ * This file is part of Qc Widgets project.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ *  (c) 2022 <techno@quebec.ca>
+ *
+ ***/
 namespace Qc\QcWidgets\Widgets;
 
 use TYPO3\CMS\Dashboard\Widgets\AdditionalCssInterface;
